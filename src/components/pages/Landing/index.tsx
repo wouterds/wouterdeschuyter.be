87ce13e542452a5dea6cd0b41c7081fb073974f4
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-09-22 22:05:01 +0200

    :sparkles: Sign In & Sign Up links

--- src/components/pages/Landing/index.tsx
+++ src/components/pages/Landing/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import Link from 'next/link';
 import Layout from 'components/Layout';
 
 const Index = () => {
   return (
     <Layout>
-      <p>Hello world!</p>
+      <h1>Hello world!</h1>
+      <Link href="/admin/sign-in">
+        <a>Sign in</a>
+      </Link>
+      {' or '}
+      <Link href="/admin/sign-up">
+        <a>Sign up</a>
+      </Link>
     </Layout>
   );
 };
