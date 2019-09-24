e06517f6c22b134069026e507f1e72565dd00a46
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-09-24 13:24:30 +0200

    :alembic: Testing react-hook-form

--- src/components/pages/Admin/SignIn/index.tsx
+++ src/components/pages/Admin/SignIn/index.tsx
@@ -1,24 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Form from 'components/Form';
 import { Container } from './styles';
 import Layout from 'components/Layout';
+import useForm from 'react-hook-form';
 
 const SignIn = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const { register, handleSubmit, errors } = useForm({
+    submitFocusError: false,
+  });
+
+  const onSubmit = handleSubmit(data => {
+    setIsLoading(true);
+
+    console.log({ data });
+
+    setTimeout(() => setIsLoading(false), 1000);
+  });
+
   return (
     <Layout>
       <Container>
         <h1>Sign in</h1>
-        <Form>
+        <Form onSubmit={onSubmit}>
           <Form.Field>
             <Form.Label htmlFor="email">Email</Form.Label>
-            <Form.Input id="email" type="email" />
+            <Form.Input
+              id="email"
+              name="email"
+              type="email"
+              hasError={errors.hasOwnProperty('email')}
+              ref={register({ required: true })}
+            />
           </Form.Field>
           <Form.Field>
             <Form.Label htmlFor="password">Password</Form.Label>
-            <Form.Input id="password" type="password" />
+            <Form.Input
+              id="password"
+              name="password"
+              type="password"
+              hasError={errors.hasOwnProperty('password')}
+              ref={register({ required: true })}
+            />
           </Form.Field>
           <Form.Field>
-            <Form.Button type="submit">Sign in</Form.Button>
+            <Form.Button type="submit" isLoading={isLoading}>
+              Sign in
+            </Form.Button>
           </Form.Field>
         </Form>
       </Container>
