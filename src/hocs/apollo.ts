1a5f31931e8e2c6d80ca4ca5acc274ae97314b05
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-09-25 21:47:49 +0200

    :lock: Pass JWT token if found

--- src/hocs/apollo.ts
+++ src/hocs/apollo.ts
@@ -3,14 +3,25 @@ import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
 import fetch from 'isomorphic-unfetch';
 import withApollo from 'next-with-apollo';
+import Cookie, { Cookies } from 'services/cookie';
 
-export default withApollo(
-  ({ initialState }) =>
-    new ApolloClient({
-      link: new HttpLink({
-        uri: `${process.env.API_ENDPOINT}`,
-        fetch,
-      }),
-      cache: new InMemoryCache().restore(initialState),
+export default withApollo(({ ctx, initialState }) => {
+  if (ctx && ctx.req && ctx.req.headers && ctx.req.headers.cookie) {
+    Cookie.init(ctx.req.headers.cookie);
+  }
+
+  const headers: any = {};
+  const JWT_TOKEN = Cookie.getClient().get(Cookies.JWT);
+  if (JWT_TOKEN) {
+    headers.authorization = `Bearer ${JWT_TOKEN}`;
+  }
+
+  return new ApolloClient({
+    link: new HttpLink({
+      uri: `${process.env.API_ENDPOINT}`,
+      fetch,
+      headers,
     }),
-);
+    cache: new InMemoryCache().restore(initialState),
+  });
+});
