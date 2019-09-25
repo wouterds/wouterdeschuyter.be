5d9db6ab52f09097f664ab61e472f96e61211e2c
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-09-25 23:02:35 +0200

    :construction: Debugging link to admin

--- src/components/pages/Landing/index.tsx
+++ src/components/pages/Landing/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import Layout from 'components/Layout';
+import Link from 'next/link';
 
 const Landing = () => {
   return (
     <Layout>
       <Layout.Content>
         <h1>Hello world!</h1>
+        <p>
+          <Link href="/admin">
+            <a>Admin</a>
+          </Link>
+        </p>
       </Layout.Content>
     </Layout>
   );
