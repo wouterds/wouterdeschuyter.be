b4d4fa87ada188ada8896c3856e152678942ae6f
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-09-28 11:42:46 +0200

    :sparkles: Post list

--- src/components/pages/Admin/Posts/index.tsx
+++ src/components/pages/Admin/Posts/index.tsx
@@ -1,15 +1,92 @@
 import React from 'react';
-import Layout from 'components/Layout';
+import gql from 'graphql-tag';
+import { useQuery } from 'react-apollo';
+import { format } from 'date-fns';
 import withAuth, { AuthProps } from 'hocs/auth';
+import Layout from 'components/Layout';
 import Header from 'components/Admin/Header';
-import { backgroundColor } from './styles';
+import { backgroundColor, Content } from './styles';
+
+const FETCH_DATA = gql`
+  query fetchData {
+    posts {
+      id
+      title
+      user {
+        firstName
+        lastName
+      }
+      status
+      publishedAt
+      createdAt
+    }
+  }
+`;
 
 const Posts = (props: AuthProps) => {
+  const { loading, data } = useQuery(FETCH_DATA);
+
   return (
     <Layout backgroundColor={backgroundColor}>
       <Header {...props} active="posts" />
       <Layout.Content>
-        <h1>Posts</h1>
+        <Content>
+          <h1>List of posts</h1>
+          {loading && <h2>Loading...</h2>}
+          {!loading && (
+            <table>
+              <thead>
+                <tr>
+                  <th>Identifier</th>
+                  <th>Title</th>
+                  <th>Autor</th>
+                  <th>Status</th>
+                  <th>Published at</th>
+                  <th>Created at</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.posts.map((post: any, index: number) => (
+                  <tr key={index}>
+                    <td>
+                      <div>{post.id}</div>
+                    </td>
+                    <td>
+                      <div>{post.title}</div>
+                    </td>
+                    <td>
+                      {post.user.firstName} {post.user.lastName}
+                    </td>
+                    <td
+                      style={{
+                        color:
+                          post.status === 'PUBLISHED' ? '#2ecc71' : '#e74c3c',
+                      }}
+                    >
+                      {post.status === 'PUBLISHED' && 'Published'}
+                      {post.status === 'DELETED' && 'Deleted'}
+                      {post.status === 'DRAFT' && 'Draft'}
+                    </td>
+                    <td>
+                      {post.publishedAt
+                        ? format(
+                            new Date(parseInt(post.publishedAt)),
+                            'MMMM do yyyy, HH:mm',
+                          )
+                        : '--'}
+                    </td>
+                    <td>
+                      {format(
+                        new Date(parseInt(post.createdAt)),
+                        'MMMM do yyyy, HH:mm',
+                      )}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
+        </Content>
       </Layout.Content>
     </Layout>
   );
--- src/components/pages/Admin/Posts/styles.ts
+++ src/components/pages/Admin/Posts/styles.ts
@@ -1,1 +1,42 @@
+import styled from 'styled-components';
+
 export const backgroundColor = '#F3F6F8';
+
+export const Content = styled.div`
+  background: #fff;
+  border-radius: 10px;
+  padding: 25px;
+
+  h1 {
+    font-size: 1em;
+  }
+
+  table {
+    width: 100%;
+
+    th {
+      font-weight: 600;
+      text-align: left;
+      border-bottom: 2px solid #f3f6f8;
+    }
+
+    td,
+    th {
+      padding: 10px;
+      padding-left: 0;
+    }
+
+    td {
+      > div {
+        max-width: 400px;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+      }
+
+      &:first-child > div {
+        max-width: 180px;
+      }
+    }
+  }
+`;
