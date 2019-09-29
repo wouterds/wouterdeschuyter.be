fdb18873b0fb0655013725ee8dfec3490d6a1576
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-09-30 00:02:03 +0200

    :lipstick: Editorial page style

--- src/components/Layout/Content/index.tsx
+++ src/components/Layout/Content/index.tsx
@@ -4,12 +4,17 @@ import { Container } from './styles';
 interface Props {
   children: ReactNode;
   centered?: boolean;
+  editorial?: boolean;
 }
 
 const Content = (props: Props) => {
-  const { children, centered } = props;
+  const { children, centered, editorial } = props;
 
-  return <Container centered={centered}>{children}</Container>;
+  return (
+    <Container centered={centered} editorial={editorial}>
+      {children}
+    </Container>
+  );
 };
 
 export default Content;
--- src/components/Layout/Content/styles.ts
+++ src/components/Layout/Content/styles.ts
@@ -2,13 +2,33 @@ import styled, { css } from 'styled-components';
 
 export const backgroundColor = '#F3F6F8';
 
-export const Container = styled.div<{ centered?: boolean }>`
+export const Container = styled.div<{
+  centered?: boolean;
+  editorial?: boolean;
+}>`
   padding: 35px;
+  font-size: 1.2em;
 
   ${({ centered }) =>
     centered &&
     css`
-      max-width: 960px;
+      max-width: 1000px;
       margin: 0 auto;
+    `}
+
+  ${({ editorial }) =>
+    editorial &&
+    css`
+      h1 {
+        &:after {
+          content: '';
+          display: block;
+          background: #2b2f33;
+          height: 0.1em;
+          width: 2em;
+          margin-top: 0.5em;
+          margin-bottom: 1em;
+        }
+      }
     `}
 `;
