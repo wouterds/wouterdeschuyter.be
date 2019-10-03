b924cd21c32c1e4a22f8e446dbb8142a8ed9dd60
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-10-03 09:43:11 +0200

    :children_crossing: Toast it!

--- src/components/Pages/Contact/index.tsx
+++ src/components/Pages/Contact/index.tsx
@@ -3,6 +3,7 @@ import ReCAPTCHA from 'react-google-recaptcha';
 import useForm from 'react-hook-form';
 import gql from 'graphql-tag';
 import { useMutation } from 'react-apollo';
+import { toast } from 'react-toastify';
 import Layout from 'components/Layout';
 import Header from 'components/Header';
 import Footer from 'components/Footer';
@@ -56,13 +57,26 @@ const Contact = () => {
 
     contact({ variables: { fullName, email, subject, message } })
       .then(() => {
+        toast(
+          "Your message has been sent, I'll get back to you as soon as possible!",
+          { type: 'success' },
+        );
         reset();
       })
+      .catch(() =>
+        toast('Something went wrong while trying to send your message 😟', {
+          type: 'error',
+        }),
+      )
       .finally(() => setIsLoading(false));
   }, [contact, data, reset]);
 
   const onError = () => {
     setIsLoading(false);
+
+    toast('ReCAPTCHA failed 🤔...', {
+      type: 'error',
+    });
   };
 
   return (
