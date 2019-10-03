c67b570644a1002f0d67c43f1b491ffb1e97ef28
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-10-03 21:25:46 +0200

    :mag: Use 404 for invalid pages

--- src/components/Pages/Blog/index.tsx
+++ src/components/Pages/Blog/index.tsx
@@ -32,31 +32,57 @@ const Blog = (props: Props) => {
   return (
     <Layout>
       <Header />
-      <Layout.Content centered>
-        <Container>{posts && <Posts posts={posts} />}</Container>
+      <Layout.Content centered editorial={posts.length === 0}>
+        <Container>
+          {posts.length === 0 && (
+            <>
+              <h1>The end</h1>
+              <p>
+                Looks like you reached the end of my blog, no more posts here!
+              </p>
+            </>
+          )}
+          {posts.length > 0 && <Posts posts={posts} />}
+        </Container>
       </Layout.Content>
       <Footer centered />
     </Layout>
   );
 };
 
-Blog.getInitialProps = async ({ query, apolloClient }: NextPageContext) => {
+Blog.getInitialProps = async ({
+  query,
+  apolloClient,
+  res,
+}: NextPageContext) => {
   let { page = 1 } = query;
 
   page = Number(page);
   if (Number.isNaN(page)) {
     page = 1;
+
+    if (res) {
+      res.statusCode = 404;
+    }
   }
 
   page = page - 1;
   if (page < 0) {
     page = 0;
+
+    if (res) {
+      res.statusCode = 404;
+    }
   }
 
   const { posts } = (await apolloClient.query({
     query: FETCH_DATA,
     variables: { limit: POSTS_PER_PAGE, offset: page * POSTS_PER_PAGE },
   })).data;
+
+  if (posts.length === 0 && res) {
+    res.statusCode = 404;
+  }
 
   return { page, posts };
 };
