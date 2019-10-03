0eb387f39632736d066089e20c9c34244e5c0075
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-10-03 20:18:07 +0200

    :children_crossings: More posts link

--- src/components/Pages/Landing/index.tsx
+++ src/components/Pages/Landing/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import gql from 'graphql-tag';
 import { useQuery } from 'react-apollo';
+import Link from 'next/link';
 import Layout from 'components/Layout';
 import Header from 'components/Header';
 import Footer from 'components/Footer';
 import Posts from 'components/Posts';
 import LandingHeader from './Header';
+import { MoreLink, Links } from './styles';
 
 const FETCH_DATA = gql`
   query fetchData {
@@ -28,6 +30,12 @@ const Landing = () => {
       <Layout.Content centered>
         <LandingHeader />
         {data && <Posts posts={data.posts} />}
+
+        <Links>
+          <Link href="/blog" passHref>
+            <MoreLink>More posts</MoreLink>
+          </Link>
+        </Links>
       </Layout.Content>
       <Footer centered />
     </Layout>
---
+++ src/components/Pages/Landing/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Links = styled.div`
+  text-align: right;
+  margin-top: 2.5em;
+`;
+
+export const MoreLink = styled.a`
+  font-size: 0.9em;
+  color: #48545e !important;
+`;
