5d721da2b04fbda9630d42f652ec32c3c0dd18e9
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-10-04 17:40:36 +0200

    :sparkles: Render media images

--- src/components/Markdown/index.tsx
+++ src/components/Markdown/index.tsx
@@ -1,15 +1,82 @@
 import React from 'react';
 import marked from 'marked';
+import { useQuery } from 'react-apollo';
+import gql from 'graphql-tag';
 import { Container } from './styles';
 
 interface Props {
   markdown: string;
 }
 
-const Markdown = (props: Props) => {
-  const { markdown } = props;
+const FETCH_MEDIA = gql`
+  query fetchData($ids: [String]) {
+    mediaAssets(ids: $ids) {
+      id
+      name
+      fileName
+      url
+      width
+      height
+    }
+  }
+`;
 
-  <Container dangerouslySetInnerHTML={{ __html: marked(markdown) }} />;
+const extractMediaIds = (text: string): string[] => {
+  const matches = text.match(new RegExp(/\:media\:(.+)\:/g));
+
+  if (!matches) {
+    return [];
+  }
+
+  return matches.map(match => match.replace(':media:', '').replace(':', ''));
+};
+
+const generateHtmlFromMarkdown = (
+  markdown: string,
+  mediaAssets: Array<{
+    id: string;
+    name: string;
+    fileName?: string;
+    url?: string;
+    width: number;
+    height: number;
+  }>,
+): string => {
+  let html = marked(markdown);
+
+  for (const mediaAsset of mediaAssets) {
+    if (mediaAsset.fileName) {
+      html = html.replace(
+        `:media:${mediaAsset.id}:`,
+        `<img src="/static/media/${mediaAsset.fileName}" />`,
+      );
+    }
+
+    if (mediaAsset.url) {
+      const url = new URL(mediaAsset.url);
+
+      if (['youtube.com', 'youtu.be'].includes(url.hostname)) {
+        const youtubeId = url.pathname.split('/').pop();
+
+        html = html.replace(
+          `:media:${mediaAsset.id}:`,
+          `<iframe src="https://youtube.com/embed/${youtubeId}" frameborder="0" allowfullscreen>${url}</iframe>`,
+        );
+      }
+    }
+  }
+
+  return html;
+};
+
+const Markdown = ({ markdown }: Props) => {
+  const mediaIds = extractMediaIds(markdown);
+  const { data } = useQuery(FETCH_MEDIA, { variables: { ids: mediaIds } });
+  const mediaAssets = (data && data.mediaAssets) || [];
+
+  const html = generateHtmlFromMarkdown(markdown, mediaAssets);
+
+  return <Container dangerouslySetInnerHTML={{ __html: html }} />;
 };
 
 export default Markdown;
