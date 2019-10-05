b14ad890e3a0bfbf391ff67ebb73310e23d768c1
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-10-05 23:21:18 +0200

    :sparkles: Dynamic sitemap

--- src/api/sitemap.ts
+++ src/api/sitemap.ts
@@ -4,14 +4,38 @@ import {
   ISitemapItemOptionsLoose,
   EnumChangefreq,
 } from 'sitemap';
+import Apollo from 'services/apollo';
+import gql from 'graphql-tag';
 
-export default (_req: NextApiRequest, res: NextApiResponse) => {
+const FETCH_POSTS = gql`
+  query fetchData {
+    posts {
+      slug
+      publishedAt
+    }
+  }
+`;
+
+export default async (_req: NextApiRequest, res: NextApiResponse) => {
   const urls: ISitemapItemOptionsLoose[] = [
     { url: '/', changefreq: EnumChangefreq.DAILY, priority: 1 },
     { url: '/about', changefreq: EnumChangefreq.WEEKLY, priority: 0.7 },
     { url: '/blog', changefreq: EnumChangefreq.DAILY, priority: 0.7 },
     { url: '/contact', changefreq: EnumChangefreq.WEEKLY, priority: 0.7 },
   ];
+
+  const apolloClient = Apollo.getClient();
+
+  const { posts } = (await apolloClient.query({ query: FETCH_POSTS })).data;
+
+  for (const post of posts) {
+    urls.push({
+      url: `/blog/${post.slug}`,
+      changefreq: EnumChangefreq.WEEKLY,
+      priority: 0.5,
+      lastmodISO: new Date(parseInt(post.publishedAt)).toISOString(),
+    });
+  }
 
   const sitemap = createSitemap({
     hostname: process.env.URL,
