9528bf5cc84131780c4b9a8de3f3609581488e71
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-10-05 23:40:02 +0200

    :zap: Sitemap service

--- src/api/sitemap.ts
+++ src/api/sitemap.ts
@@ -1,47 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import {
-  createSitemap,
-  ISitemapItemOptionsLoose,
-  EnumChangefreq,
-} from 'sitemap';
-import Apollo from 'services/apollo';
-import gql from 'graphql-tag';
-
-const FETCH_POSTS = gql`
-  query fetchData {
-    posts {
-      slug
-      publishedAt
-    }
-  }
-`;
+import Sitemap from 'services/sitemap';
 
 export default async (_req: NextApiRequest, res: NextApiResponse) => {
-  const urls: ISitemapItemOptionsLoose[] = [
-    { url: '/', changefreq: EnumChangefreq.DAILY, priority: 1 },
-    { url: '/about', changefreq: EnumChangefreq.WEEKLY, priority: 0.7 },
-    { url: '/blog', changefreq: EnumChangefreq.DAILY, priority: 0.7 },
-    { url: '/contact', changefreq: EnumChangefreq.WEEKLY, priority: 0.7 },
-  ];
-
-  const apolloClient = Apollo.getClient();
-
-  const { posts } = (await apolloClient.query({ query: FETCH_POSTS })).data;
-
-  for (const post of posts) {
-    urls.push({
-      url: `/blog/${post.slug}`,
-      changefreq: EnumChangefreq.WEEKLY,
-      priority: 0.5,
-      lastmodISO: new Date(parseInt(post.publishedAt)).toISOString(),
-    });
-  }
-
-  const sitemap = createSitemap({
-    hostname: process.env.URL,
-    urls,
-  });
-
   res.setHeader('Content-Type', 'application/xml');
-  res.send(sitemap.toXML());
+  res.send((await Sitemap.build()).getXml());
 };
---
+++ src/services/sitemap.ts
@@ -0,0 +1,67 @@
+import { createSitemap, EnumChangefreq } from 'sitemap';
+import Apollo from 'services/apollo';
+import gql from 'graphql-tag';
+
+const FETCH_POSTS = gql`
+  query fetchData {
+    posts {
+      slug
+      publishedAt
+    }
+  }
+`;
+
+class Sitemap {
+  private sitemap = createSitemap({ hostname: process.env.URL });
+
+  private buildStatic = () => {
+    this.sitemap.add({
+      url: '/',
+      changefreq: EnumChangefreq.DAILY,
+      priority: 1,
+    });
+    this.sitemap.add({
+      url: '/about',
+      changefreq: EnumChangefreq.WEEKLY,
+      priority: 0.7,
+    });
+    this.sitemap.add({
+      url: '/blog',
+      changefreq: EnumChangefreq.DAILY,
+      priority: 0.7,
+    });
+    this.sitemap.add({
+      url: '/contact',
+      changefreq: EnumChangefreq.WEEKLY,
+      priority: 0.7,
+    });
+  };
+
+  private buildDynamic = async () => {
+    const apolloClient = Apollo.getClient();
+
+    const { posts } = (await apolloClient.query({ query: FETCH_POSTS })).data;
+
+    for (const post of posts) {
+      this.sitemap.add({
+        url: `/blog/${post.slug}`,
+        changefreq: EnumChangefreq.WEEKLY,
+        priority: 0.5,
+        lastmodISO: new Date(parseInt(post.publishedAt)).toISOString(),
+      });
+    }
+  };
+
+  public build = async () => {
+    this.buildStatic();
+    await this.buildDynamic();
+
+    return this;
+  };
+
+  public getXml = () => {
+    return this.sitemap.toXML();
+  };
+}
+
+export default new Sitemap();
