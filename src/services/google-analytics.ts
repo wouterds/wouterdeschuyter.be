09d2891eb59c0d1a6267ac19e72bf10e8c8777cb
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-10-06 19:23:04 +0200

    :sparkles: GoogleAnalytics service

---
+++ src/services/google-analytics.ts
@@ -0,0 +1,41 @@
+class GoogleAnalytics {
+  private get isSDKAvailable() {
+    if (typeof window === 'undefined') {
+      return false;
+    }
+
+    if (!window.gtag) {
+      return false;
+    }
+
+    return true;
+  }
+
+  public pageView = (path: string) => {
+    if (!this.isSDKAvailable) {
+      throw new Error('GA SDK not available');
+    }
+
+    // eslint-disable-next-line @typescript-eslint/camelcase
+    window.gtag('config', process.env.GA_TRACKING_ID, { page_path: path });
+  };
+
+  public event = (
+    action: string,
+    category: string,
+    label: string,
+    value: string,
+  ) => {
+    if (!this.isSDKAvailable) {
+      throw new Error('GA SDK not available');
+    }
+
+    window.gtag('event', action, {
+      event_category: category, // eslint-disable-line @typescript-eslint/camelcase
+      event_label: label, // eslint-disable-line @typescript-eslint/camelcase
+      value,
+    });
+  };
+}
+
+export default new GoogleAnalytics();
