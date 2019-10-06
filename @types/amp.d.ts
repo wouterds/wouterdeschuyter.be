86c5397a7278a2141772d762aa5e74d17694ccb1
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-10-06 02:12:30 +0200

    :label: amp-img type

---
+++ @types/amp.d.ts
@@ -0,0 +1,13 @@
+declare namespace JSX {
+  interface AmpImg {
+    alt?: string;
+    src?: string;
+    width?: string;
+    height?: string;
+    layout?: string;
+  }
+
+  interface IntrinsicElements {
+    'amp-img': AmpImg;
+  }
+}
