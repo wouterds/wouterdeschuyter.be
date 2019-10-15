9431168b69105e6b217cf30b38952859334a190a
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-10-15 09:54:36 +0200

    :sparkles: Group likes

--- src/components/Webmentions/index.tsx
+++ src/components/Webmentions/index.tsx
@@ -67,11 +67,48 @@ const Webmentions = (props: Props) => {
     return null;
   }
 
+  const likes = webmentions.filter(
+    webmention => webmention['wm-property'] === 'like-of',
+  );
+
+  const other = webmentions.filter(
+    webmention => webmention['wm-property'] !== 'like-of',
+  );
+
   return (
     <Container>
       <h2>Webmentions</h2>
       <ul>
-        {webmentions.map((webmention: Webmention, index: number) => (
+        {likes.length > 0 && (
+          <li>
+            <a
+              className="likes"
+              target="_blank"
+              rel="noopener noreferrer"
+              href={likes[0].url}
+            >
+              <div
+                className="heads"
+                style={{ paddingLeft: `${likes.length * 1.25 + 1.75}em` }}
+              >
+                {likes
+                  .reverse()
+                  .map((webmention: Webmention, index: number) => (
+                    <img
+                      style={{ left: `${index * 1.25}em` }}
+                      key={index}
+                      src={webmention.author.photo}
+                      alt={webmention.author.name}
+                    />
+                  ))}
+              </div>
+              Liked by <strong>{likes.reverse()[0].author.name}</strong>
+              {likes.length === 2 && 'and 1 other'}
+              {likes.length > 2 && `and ${likes.length - 1} others`}
+            </a>
+          </li>
+        )}
+        {other.map((webmention: Webmention, index: number) => (
           <li key={index}>
             <a
               className="author"
@@ -88,7 +125,6 @@ const Webmentions = (props: Props) => {
               rel="noopener noreferrer"
               href={webmention.url}
             >
-              {webmention['wm-property'] === 'like-of' && 'liked'}
               {webmention['wm-property'] === 'mention-of' && 'mentioned'}
               {webmention['wm-property'] === 'in-reply-to' && 'replied'}
               {webmention['wm-property'] === 'repost-of' && 'retweeted'}
