b7138f6c8a12e80455c8fa2fec788a9ea578256f
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-10-20 20:29:17 +0200

    :wrench: Block Google in non production mode

--- src/api/robots.ts
+++ src/api/robots.ts
@@ -3,11 +3,15 @@ import { NextApiRequest, NextApiResponse } from 'next';
 export default (_req: NextApiRequest, res: NextApiResponse) => {
   let body = '';
   body += 'User-Agent: *\n';
-  body += 'Allow: /\n';
-  body += 'Disallow: /admin\n';
-  body += 'Disallow: /admin/*\n';
-  body += '\n';
-  body += `Sitemap: ${process.env.URL}/sitemap.xml\n`;
+  if (process.env.ENV === 'production') {
+    body += 'Allow: /\n';
+    body += 'Disallow: /admin\n';
+    body += 'Disallow: /admin/*\n';
+    body += '\n';
+    body += `Sitemap: ${process.env.URL}/sitemap.xml\n`;
+  } else {
+    body += 'Disallow: /\n';
+  }
 
   res.setHeader('Content-Type', 'text/plain');
   res.send(body);
