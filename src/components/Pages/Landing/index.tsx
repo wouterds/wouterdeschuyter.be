0167df36771adf8c0f75bd9c8b5e0f6390f50807
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-11-27 12:46:52 +0100

    :pencil2: Adjusted meta description

--- src/components/Pages/Landing/index.tsx
+++ src/components/Pages/Landing/index.tsx
@@ -33,7 +33,7 @@ const Landing = () => {
     <Layout>
       <Meta
         title="Wouter De Schuyter"
-        description="An ambitious, passionate Full Stack Developer."
+        description="An ambitious, passionate Full Stack Developer that loves to make digital products."
         extra={
           <script
             type="application/ld+json"
