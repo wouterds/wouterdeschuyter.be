a75659789246be54aded7378c85f421f9518b988
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-01-27 21:35:08 +0100

    :art: Doesn't need to be a private method

--- src/services/apollo.ts
+++ src/services/apollo.ts
@@ -4,35 +4,35 @@ import { HttpLink } from 'apollo-link-http';
 import fetch from 'isomorphic-unfetch';
 import Cookie, { Cookies } from 'services/cookie';
 
+const createApolloClient = (initialState = {}, jwtToken?: string) => {
+  const JWT_TOKEN = jwtToken || Cookie.get(Cookies.JWT);
+
+  return new ApolloClient({
+    ssrMode: typeof window === 'undefined',
+    link: new HttpLink({
+      uri: `${process.env.API_ENDPOINT}`,
+      fetch,
+      headers: JWT_TOKEN ? { authorization: `Bearer ${JWT_TOKEN}` } : {},
+    }),
+    cache: new InMemoryCache().restore(initialState),
+  });
+};
+
 class Apollo {
   private client: ApolloClient<any>;
 
   constructor() {
-    this.client = this.createApolloClient();
+    this.client = createApolloClient();
   }
 
   public init(initialState = {}, jwtToken?: string) {
-    this.client = this.createApolloClient(initialState, jwtToken);
+    this.client = createApolloClient(initialState, jwtToken);
 
     return this.client;
   }
 
   public getClient() {
     return this.client;
-  }
-
-  private createApolloClient(initialState = {}, jwtToken?: string) {
-    const JWT_TOKEN = jwtToken || Cookie.get(Cookies.JWT);
-
-    return new ApolloClient({
-      ssrMode: typeof window === 'undefined',
-      link: new HttpLink({
-        uri: `${process.env.API_ENDPOINT}`,
-        fetch,
-        headers: JWT_TOKEN ? { authorization: `Bearer ${JWT_TOKEN}` } : {},
-      }),
-      cache: new InMemoryCache().restore(initialState),
-    });
   }
 }
 
