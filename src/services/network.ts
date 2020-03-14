157021d7fc83419a94e21189bebcb92861ef4819
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-03-14 23:51:38 +0100

    :sparkles: NetworkService

---
+++ src/services/network.ts
@@ -0,0 +1,60 @@
+import ApolloClient from 'apollo-client';
+import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { setContext } from 'apollo-link-context';
+import fetch from 'isomorphic-unfetch';
+import Cookie, { Cookies } from 'services/cookie';
+
+class NetworkService {
+  private _apollo: ApolloClient<NormalizedCacheObject>;
+
+  public constructor() {
+    this._apollo = NetworkService.createApolloClient();
+  }
+
+  private static createApolloClient = (cache = {}) => {
+    const httpLink = new HttpLink({
+      uri: process.env.API_ENDPOINT,
+      fetch,
+    });
+
+    const authMiddleware = setContext(async (_request, context) => {
+      const jwt = Cookie.get(Cookies.JWT); // todo, get from cookie
+
+      if (!jwt) {
+        return context;
+      }
+
+      const { headers } = context;
+
+      return {
+        headers: {
+          ...headers,
+          authorization: `Bearer ${jwt}`,
+        },
+      };
+    });
+
+    const link = authMiddleware.concat(httpLink);
+
+    return new ApolloClient({
+      link,
+      cache: new InMemoryCache().restore(cache),
+      connectToDevTools: process.env.NODE_ENV !== 'production',
+    });
+  };
+
+  public init = (cache: {}) => {
+    this._apollo = NetworkService.createApolloClient(cache);
+
+    return this._apollo;
+  };
+
+  public get apollo() {
+    return this._apollo;
+  }
+}
+
+const Network = new NetworkService();
+
+export default Network;
