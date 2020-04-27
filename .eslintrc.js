fc6d02b99b671746b9c0ecc7b84d262aad1e87d3
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-04-27 09:34:43 +0200

    :wrench: Clean up eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,36 @@
 module.exports = {
+  root: true,
   parser: '@typescript-eslint/parser',
-  extends: [
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended',
-  ],
-  plugins: [
-    'react-hooks',
-    'simple-import-sort',
-  ],
-  rules: {
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-inferrable-types': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-    '@typescript-eslint/no-unused-vars': 'error',
-    'simple-import-sort/sort': 'error',
+  env: {
+    browser: true,
   },
   settings:  {
     react:  {
       version:  'detect',
     },
+  },
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'react-hooks',
+    'prettier',
+    'simple-import-sort',
+  ],
+  extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'prettier/react',
+    'prettier/standard',
+  ],
+  rules: {
+    'no-unused-vars': 'error',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-empty-function': 'warn',
+    '@typescript-eslint/ban-ts-ignore': 'warn',
+    'simple-import-sort/sort': 'error',
   },
 };
