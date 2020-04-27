30e4f69788ae0439f6ef3f17d628910111145e87
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-04-27 10:13:05 +0200

    :fire: Disable experiments bar for now

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,10 +1,10 @@
-import ExperimentsBar from 'components/ExperimentsBar';
+// import ExperimentsBar from 'components/ExperimentsBar';
 // import { useCookie, Cookies } from 'hooks/useCookie';
-import { useAmp } from 'next/amp';
-import { useRouter } from 'next/router';
-import React, { useEffect, useState } from 'react';
-import Twemoji from 'react-twemoji';
+// import { useAmp } from 'next/amp';
+// import { useRouter } from 'next/router';
+import React /*, { useEffect, useState }*/ from 'react';
 
+// import Twemoji from 'react-twemoji';
 import { Container } from './styles';
 
 interface Props {
@@ -13,16 +13,15 @@ interface Props {
 
 export const Footer = (props: Props) => {
   const { centered = false } = props;
-  const isAmp = useAmp();
-  const router = useRouter();
+  // const isAmp = useAmp();
+  // const router = useRouter();
   // const [isVisible, setIsVisible] = useCookie(Cookies.DATA_BAR);
-  const [isVisible, setIsVisible] = useState('false');
+  // const [isVisible, setIsVisible] = useState('false');
+  // const isExperiments = router.pathname.indexOf('/experiments') > -1;
 
-  const isExperiments = router.pathname.indexOf('/experiments') > -1;
-
-  useEffect(() => {
-    setIsVisible('true');
-  }, []);
+  // useEffect(() => {
+  //   setIsVisible('true');
+  // }, []);
 
   return (
     <>
@@ -33,7 +32,7 @@ export const Footer = (props: Props) => {
         </p>
         <p>
           Made using React, GraphQL, Node.js and a few other packages ✨
-          {isVisible !== 'true' && !isAmp && !isExperiments && (
+          {/* {isVisible !== 'true' && !isAmp && !isExperiments && (
             <Twemoji options={{ className: 'twemoji' }} noWrapper>
               <a
                 title="Ooooooooh, what would happen if you click?"
@@ -42,12 +41,12 @@ export const Footer = (props: Props) => {
                 🧪
               </a>
             </Twemoji>
-          )}
+          )} */}
         </p>
       </Container>
-      {!isAmp && (isVisible === 'true' || isExperiments) && (
+      {/* {!isAmp && (isVisible === 'true' || isExperiments) && (
         <ExperimentsBar onClose={() => setIsVisible('false')} />
-      )}
+      )} */}
     </>
   );
 };
