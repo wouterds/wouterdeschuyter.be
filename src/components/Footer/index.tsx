3b15a903c2f6b0069bbecc07800765e733b4e3df
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-04-29 21:03:09 +0200

    :fire: Delete code instead of commenting it out

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,4 @@
-import ExperimentsBar from 'components/ExperimentsBar';
-// import { useCookie, Cookies } from 'hooks/useCookie';
-import { useAmp } from 'next/amp';
-import { useRouter } from 'next/router';
-import React, { useEffect, useState } from 'react';
-import Twemoji from 'react-twemoji';
+import React from 'react';
 
 import { Container } from './styles';
 
@@ -13,42 +8,18 @@ interface Props {
 
 export const Footer = (props: Props) => {
   const { centered = false } = props;
-  const isAmp = useAmp();
-  const router = useRouter();
-  // const [isVisible, setIsVisible] = useCookie(Cookies.DATA_BAR);
-  const [isVisible, setIsVisible] = useState('false');
-
-  const isExperiments = router.pathname.indexOf('/experiments') > -1;
-
-  useEffect(() => {
-    setIsVisible('true');
-  }, []);
 
   return (
-    <>
-      <Container centered={centered}>
-        <p>
-          &copy; Copyright {new Date().getFullYear()} Wouter De Schuyter,
-          BE0745.964.642
-        </p>
-        <p>
-          Made using React, GraphQL, Node.js and a few other packages ✨
-          {isVisible !== 'true' && !isAmp && !isExperiments && (
-            <Twemoji options={{ className: 'twemoji' }} noWrapper>
-              <a
-                title="Ooooooooh, what would happen if you click?"
-                onClick={() => setIsVisible('true')}
-              >
-                🧪
-              </a>
-            </Twemoji>
-          )}
-        </p>
-      </Container>
-      {!isAmp && (isVisible === 'true' || isExperiments) && (
-        <ExperimentsBar onClose={() => setIsVisible('false')} />
-      )}
-    </>
+    <Container centered={centered}>
+      <p>
+        &copy; Copyright {new Date().getFullYear()} Wouter De Schuyter -
+        BE0745.964.642
+      </p>
+      <p>
+        Made in TypeScript using React, GraphQL, Node.js and a few other
+        packages ✨
+      </p>
+    </Container>
   );
 };
 
