abfd9e0b5674500a143809f513efb60929c9c636
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-05-03 09:15:29 +0200

    :fire: Remove admin stuff

--- src/components/Pages/Admin/Users/styles.ts
+++
@@ -1,5 +0,0 @@
-import { Content as ImportedContent } from 'components/Admin';
-import styled from 'styled-components';
-export const backgroundColor = '#F3F6F8';
-
-export const Content = styled(ImportedContent)``;
