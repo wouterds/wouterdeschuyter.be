a06bfe87dffc4fb816faa6974c185dc5000d8fff
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-06-06 11:18:17 +0200

    :fire: More unused types

--- @types/next.d.ts
+++
@@ -1,8 +0,0 @@
-import ApolloClient from 'apollo-client';
-import { NextPageContext as _NextPageContext } from 'next';
-
-declare module 'next' {
-  export interface NextPageContext extends _NextPageContext {
-    apolloClient: ApolloClient<any>;
-  }
-}
