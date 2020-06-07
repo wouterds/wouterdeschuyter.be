1f22215279b7a595d4b34053184c2ac23761d8b8
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-06-07 23:07:06 +0200

    :mag: Disable until we find a workaround for AMP pages

--- src/components/Pages/Blog/Detail/Helpers/Highlightjs.tsx
+++ src/components/Pages/Blog/Detail/Helpers/Highlightjs.tsx
@@ -1,14 +1,21 @@
 import hljs from 'highlight.js';
+import { useAmp } from 'next/amp';
 import Head from 'next/head';
 import React from 'react';
 import { useEffect } from 'react';
 
 const HighlightJS = () => {
+  const isAmp = useAmp();
+
   useEffect(() => {
     document
       .querySelectorAll('pre code')
       .forEach((block) => hljs.highlightBlock(block));
   }, []);
+
+  if (isAmp) {
+    return null;
+  }
 
   return (
     <Head>
