bb8db521a10af474852a04837337a4658f06e7b7
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-06-21 10:26:32 +0200

    :wrench: postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    ...(process.env.NODE_ENV === 'production'
+      ? {
+          '@fullhuman/postcss-purgecss': {
+            content: ['./src/pages/**/*.ts', './src/components/**/*.ts'],
+            defaultExtractor: (content) =>
+              content.match(/[\w-/:]+(?<!:)/g) || [],
+          },
+        }
+      : {}),
+  },
+};
