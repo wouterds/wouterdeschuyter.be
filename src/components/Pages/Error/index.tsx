c5ebc0a465ef0b3cf47372a5efb8fd8f90e0e87d
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-06-22 08:36:00 +0200

    :recycle: Tailwind.css

--- src/components/Pages/Error/index.tsx
+++ src/components/Pages/Error/index.tsx
@@ -13,7 +13,7 @@ const Error = () => (
       extra={<meta name="robots" content="noindex, follow" />}
     />
     <Header />
-    <Layout.Content centered editorial>
+    <Layout.Content editorial>
       <div className={styles.error}>
         <h1>Whoops!</h1>
         <p>It looks like something went wrong 😢...</p>
@@ -21,7 +21,7 @@ const Error = () => (
         <img src="/static/error.gif" alt="error.gif" />
       </div>
     </Layout.Content>
-    <Footer centered />
+    <Footer />
   </Layout>
 );
 
