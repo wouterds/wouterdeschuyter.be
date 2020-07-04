3d1c158b2d20b0a736c689e3fcaaf7eec4646eb9
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-07-04 22:04:29 +0200

    :lipstick: Layout dark mode

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,3 +1,4 @@
+import cx from 'classnames';
 import React, { ReactNode } from 'react';
 import Twemoji from 'react-twemoji';
 
@@ -5,15 +6,23 @@ import Content from './Content';
 import styles from './styles.module.css';
 
 interface Props {
+  dark?: boolean;
   children: ReactNode;
 }
 
 const Layout = (props: Props) => {
-  const { children } = props;
+  const { dark = false, children } = props;
 
   return (
     <Twemoji options={{ className: styles.twemoji }} noWrapper>
-      <div className={styles.layout}>{children}</div>
+      <div
+        className={cx({
+          [styles.layout]: true,
+          [styles.dark]: dark,
+        })}
+      >
+        {children}
+      </div>
     </Twemoji>
   );
 };
