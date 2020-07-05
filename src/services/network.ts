74a7f36b4067d851f2e35c4dd36363ab172ddf39
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-07-05 10:07:11 +0200

    :recycle: Reworked Network service

--- src/services/network.ts
+++ src/services/network.ts
@@ -1,32 +1,57 @@
 import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
 import { ApolloClient } from 'apollo-client';
+import { setContext } from 'apollo-link-context';
 import { HttpLink } from 'apollo-link-http';
 
-const createApolloClient = (cache = {}) => {
-  const link = new HttpLink({
-    uri: process.env.NEXT_PUBLIC_API_URL,
-  });
+import CookieService, { Cookies } from './cookie';
 
-  return new ApolloClient({
-    link,
-    cache: new InMemoryCache().restore(cache),
-    connectToDevTools: process.env.NODE_ENV !== 'production',
-  });
-};
+class NetworkService {
+  private _apollo: ApolloClient<NormalizedCacheObject>;
 
-let apollo: ApolloClient<NormalizedCacheObject> = createApolloClient();
+  public constructor() {
+    this._apollo = this.createApolloClient();
+  }
 
-const init = (cache = {}) => {
-  apollo = createApolloClient(cache);
+  private createApolloClient = (cache = {}) => {
+    const httpLink = new HttpLink({
+      uri: process.env.NEXT_PUBLIC_API_URL,
+    });
 
-  return apollo;
-};
+    const authMiddleware = setContext(async (_request, context) => {
+      const jwt = CookieService.get(Cookies.JWT);
 
-const Network = {
-  init,
-  get apollo() {
-    return apollo;
-  },
-};
+      const { headers = {} } = context;
+
+      if (jwt) {
+        headers['authorization'] = `Bearer ${jwt}`;
+      }
+
+      return {
+        ...context,
+        headers,
+      };
+    });
+
+    const link = authMiddleware.concat(httpLink);
+
+    return new ApolloClient({
+      link,
+      cache: new InMemoryCache().restore(cache),
+      connectToDevTools: process.env.NODE_ENV !== 'production',
+    });
+  };
+
+  public init = (cache = {}) => {
+    this._apollo = this.createApolloClient(cache);
+
+    return this._apollo;
+  };
+
+  public get apollo() {
+    return this._apollo;
+  }
+}
+
+const Network = new NetworkService();
 
 export default Network;
