4d693db7dd4446136302da0acab02c93a37aa41e
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-07-05 09:58:42 +0200

    :construction: Admin page

---
+++ src/components/Pages/Admin/index.tsx
@@ -0,0 +1,23 @@
+import Footer from 'components/Footer';
+import Header from 'components/Header';
+import Layout from 'components/Layout';
+import Meta from 'components/Meta';
+import React from 'react';
+
+const Admin = () => {
+  return (
+    <Layout>
+      <Meta
+        title="Admin"
+        extra={<meta key="robots" name="robots" content="noindex, follow" />}
+      />
+      <Header />
+      <Layout.Content editorial>
+        <h1>Admin</h1>
+      </Layout.Content>
+      <Footer />
+    </Layout>
+  );
+};
+
+export default Admin;
