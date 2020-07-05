f7e3db7b81284d8b2bf8e77ee34c70e533305c4b
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-07-05 11:06:36 +0200

    :sparkles: Button loading state

--- src/components/Form/Button/index.tsx
+++ src/components/Form/Button/index.tsx
@@ -17,6 +17,7 @@ interface Props
   > {
   isLoading?: boolean;
   onClick?: (e: MouseEvent<HTMLButtonElement>) => void;
+  children: ReactNode;
 }
 
 const Button = (props: Props, ref: Ref<HTMLButtonElement>) => {
@@ -46,7 +47,24 @@ const Button = (props: Props, ref: Ref<HTMLButtonElement>) => {
         [styles.button]: true,
         [styles.loading]: isLoading,
       })}
-    />
+    >
+      <span className={styles.content}>{props.children}</span>
+
+      <span className={styles.spinner}>
+        <svg className="h-8 w-8 opacity-75" viewBox="0 0 24 24">
+          <path
+            className="text-gray-600"
+            fill="currentColor"
+            d="M12 21a9 9 0 100-18 9 9 0 000 18zm0-2a7 7 0 110-14 7 7 0 010 14z"
+          />
+          <path
+            className="text-gray-400"
+            fill="currentColor"
+            d="M12 3a9 9 0 010 18v-2a7 7 0 000-14V3z"
+          />
+        </svg>
+      </span>
+    </button>
   );
 };
 
