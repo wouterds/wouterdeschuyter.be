7f3e34d6971a07852825b86be0c50b94ee3d0662
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-07-05 20:09:04 +0200

    :fire: Remove useless favicon crap

--- src/components/Meta.tsx
+++ src/components/Meta.tsx
@@ -53,11 +53,6 @@ const Meta = (props: Props) => {
       {extra}
       <title>{title}</title>
       <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/apple-touch-icon.png"
-      />
-      <link
         rel="icon"
         type="image/png"
         sizes="32x32"
@@ -69,10 +64,6 @@ const Meta = (props: Props) => {
         sizes="16x16"
         href="/favicon-16x16.png"
       />
-      <link rel="manifest" href="/site.webmanifest" />
-      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#191f2b" />
-      <meta name="msapplication-TileColor" content="#191f2b" />
-      <meta name="theme-color" content="#ffffff" />
       <link
         rel="alternate"
         type="application/rss+xml"
