7ad3c58e10f274a4f9bc4cbc43bd682fb7f480a4
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-07-05 15:16:19 +0200

    :art: Simplify admin render

--- src/components/Pages/Admin/index.tsx
+++ src/components/Pages/Admin/index.tsx
@@ -7,41 +7,29 @@ import React from 'react';
 
 import styles from './styles.module.css';
 
-interface Props {
-  user: {
-    id: string;
-    name: string;
-    email: string;
-  };
-}
-
-const Admin = (props: Props) => {
-  const { user } = props;
-
-  return (
-    <Layout>
-      <Meta
-        title="Admin"
-        extra={<meta key="robots" name="robots" content="noindex, follow" />}
-      />
-      <Header />
-      <Layout.Content fullWidth>
-        <div className={styles.admin}>
-          <nav className={styles.nav}>
-            <a href="#">Dashboard</a>
-            <a href="#">Users</a>
-            <a href="#">Media assets</a>
-            <a href="#">Posts</a>
-            <a href="#">Post aliases</a>
-            <a href="#">Authentication requests</a>
-            <a href="#">Access tokens</a>
-            <a href="#">Sensors</a>
-          </nav>
-        </div>
-      </Layout.Content>
-      <Footer fullWidth />
-    </Layout>
-  );
-};
+const Admin = () => (
+  <Layout>
+    <Meta
+      title="Admin"
+      extra={<meta key="robots" name="robots" content="noindex, follow" />}
+    />
+    <Header />
+    <Layout.Content fullWidth>
+      <div className={styles.admin}>
+        <nav className={styles.nav}>
+          <a href="#">Dashboard</a>
+          <a href="#">Users</a>
+          <a href="#">Media assets</a>
+          <a href="#">Posts</a>
+          <a href="#">Post aliases</a>
+          <a href="#">Authentication requests</a>
+          <a href="#">Access tokens</a>
+          <a href="#">Sensors</a>
+        </nav>
+      </div>
+    </Layout.Content>
+    <Footer fullWidth />
+  </Layout>
+);
 
 export default withAuth(Admin as any);
