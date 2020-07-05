ee6cfd31d12f52827fdf526b212a59d51c6eb4fc
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-07-05 13:51:31 +0200

    :mag: Blacklist admin pages

--- src/api/robots.ts
+++ src/api/robots.ts
@@ -7,6 +7,8 @@ export default (_req: NextApiRequest, res: NextApiResponse) => {
   body += 'Allow: /\n';
   body += 'Disallow: /ping\n';
   body += 'Disallow: /experiments/connect-spotify\n';
+  body += 'Disallow: /admin\n';
+  body += 'Disallow: /admin/*\n';
   body += 'Disallow: /api/*\n\n';
   body += `Sitemap: ${process.env.NEXT_PUBLIC_APP_URL}/sitemap.xml.gz\n`;
 
