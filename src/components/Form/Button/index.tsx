734c055e26b46efda1d9ceca940370ca4b5dbdca
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-07-06 07:45:25 +0200

    :art: Move styles to css

--- src/components/Form/Button/index.tsx
+++ src/components/Form/Button/index.tsx
@@ -52,17 +52,12 @@ const Button = (props: Props, ref: Ref<HTMLButtonElement>) => {
       <span className={styles.content}>{props.children}</span>
 
       <span className={styles.spinner}>
-        <svg className="h-8 w-8 opacity-75" viewBox="0 0 24 24">
+        <svg viewBox="0 0 24 24">
           <path
-            className="text-gray-600"
             fill="currentColor"
             d="M12 21a9 9 0 100-18 9 9 0 000 18zm0-2a7 7 0 110-14 7 7 0 010 14z"
           />
-          <path
-            className="text-gray-400"
-            fill="currentColor"
-            d="M12 3a9 9 0 010 18v-2a7 7 0 000-14V3z"
-          />
+          <path fill="currentColor" d="M12 3a9 9 0 010 18v-2a7 7 0 000-14V3z" />
         </svg>
       </span>
     </button>
